4aba043a705405809d228041a66b1853daa6c4cc
YuTaNCCU <ayuttyua@gmail.com>
2017-08-06 11:45:04 +0800

    1

---
+++ categorical_data.R
@@ -0,0 +1,23 @@
+# Data Preprocessing
+
+# Importing the dataset
+dataset = read.csv('Data.csv')
+
+# Taking care of missing data
+#YuTa:if age is NA , then take average of x ; if age is not NA, then copy the same value
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+# Encoding categorical data
+#YuTA: Transform the string into factor(1,2,3....)
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1, 2, 3))
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('No', 'Yes'),
+                           labels = c(0, 1))
+
